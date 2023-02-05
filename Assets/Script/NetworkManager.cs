28e93bbeb1aa90c1e2a6a8fb7ba88f34b97645fa
Shinrobin <shinrobin97@naver.com>
2023-02-06 01:17:05 +0900

    서버 접속 연동 완료

--- Assets/Script/NetworkManager.cs
+++ Assets/Script/NetworkManager.cs
@@ -20,7 +20,7 @@ public class NetworkManager : MonoBehaviour
 		// DNS (Domain Name System)
 		string host = Dns.GetHostName();
 		IPHostEntry ipHost = Dns.GetHostEntry(host);
-		IPAddress ipAddr = ipHost.AddressList[0];
+		IPAddress ipAddr = IPAddress.Parse("192.168.219.100");
 		IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 		Connector connector = new Connector();
